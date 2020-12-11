de710421b3b07d8e24581e07d9e7dc3b95b41bc9
Evgeniy <47885510+EvgeniyAfl@users.noreply.github.com>
2020-12-11 15:06:14 +0300

    adding header template

--- src/plugins/elements/index.js
+++ src/plugins/elements/index.js
@@ -9,7 +9,8 @@ import {
   FormItem,
   Input,
   Select,
-  Option
+  Option,
+  Alert
 } from 'element-ui';
 
 const elements = [
@@ -19,9 +20,10 @@ const elements = [
   FormItem,
   Input,
   Select,
-  Option
+  Option,
+  Alert
 ];
 
 locale.use(lang);
 
-elements.forEach(El => Vue.use(El, { locate }));
+elements.forEach(El => Vue.use(El, { locale }));
